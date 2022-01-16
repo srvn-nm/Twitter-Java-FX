86693f5193b1fdec248f749cb450c28226812732
Abtinz <abtinzandi@gmail.com>
2022-01-16 19:55:33 +0330

    Java Fx Final Project

---
+++ Twitter Java FX/src/main/java/com/fxcode/twitterjavafx/FirstMenu.java
@@ -0,0 +1,21 @@
+package com.fxcode.twitterjavafx;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+
+public class FirstMenu{
+
+    @FXML
+    private VBox LogInWindow;
+
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    void onHelloButtonClick(ActionEvent event) {
+
+    }
+
+}
