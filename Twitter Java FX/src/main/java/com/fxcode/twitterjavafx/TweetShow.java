884f81319ffa95335a081dab5ece9da734f34895
Sarvin Nami <srvn0nm@gmail.com>
2022-01-26 18:36:49 +0330

    adding two new fxmls

--- Twitter Java FX/src/main/java/com/fxcode/twitterjavafx/TweetShow.java
+++ Twitter Java FX/src/main/java/com/fxcode/twitterjavafx/TweetShow.java
@@ -1,4 +1,10 @@
 package com.fxcode.twitterjavafx;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
 public class TweetShow {
+    @FXML public Label userName;
+    @FXML public Label tweet;
+    @FXML public Label time;
 }
