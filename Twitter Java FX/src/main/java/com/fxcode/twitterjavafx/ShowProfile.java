bb7c2840d9575bb73d3e368c7f32357879d16e6c
Sarvin Nami <srvn0nm@gmail.com>
2022-01-26 14:56:33 +0330

    new changes to show profile

--- Twitter Java FX/src/main/java/com/fxcode/twitterjavafx/ShowProfile.java
+++ Twitter Java FX/src/main/java/com/fxcode/twitterjavafx/ShowProfile.java
@@ -3,7 +3,10 @@ package com.fxcode.twitterjavafx;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 
@@ -11,13 +14,17 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 public class ShowProfile implements Initializable {
-    @FXML private Label userNameField;
-    @FXML private Label nameField;
-    @FXML private Circle circle;
+    @FXML public AnchorPane anchorPane;
+    @FXML public VBox tweetBox;
+    @FXML public Label name;
+    @FXML public javafx.scene.shape.Circle Circle;
+    @FXML public Label bio;
+    @FXML public Label username;
+    @FXML public ScrollPane scrollPane;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        circle.setStroke(Color.SEAGREEN);
+        Circle.setStroke(Color.SEAGREEN);
         Image image;
     }
 }
