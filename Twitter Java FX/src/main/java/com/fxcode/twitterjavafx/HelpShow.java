edc621ae5947aea5f77ae7220835e9d9d0f15dd8
Sarvin Nami <srvn0nm@gmail.com>
2022-01-26 23:14:41 +0330

    new fxml

---
+++ Twitter Java FX/src/main/java/com/fxcode/twitterjavafx/HelpShow.java
@@ -0,0 +1,11 @@
+package com.fxcode.twitterjavafx;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+
+public class HelpShow {
+    @FXML public Button About;
+    @FXML public AnchorPane Anchor;
+    @FXML public Button Help;
+}
