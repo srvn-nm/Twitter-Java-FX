802c8b552b5e8c588b0eb863af6fb8b4cb63e564
Sarvin Nami <srvn0nm@gmail.com>
2022-01-27 13:35:48 +0330

    app fxml

---
+++ Twitter Java FX/src/main/java/com/fxcode/twitterjavafx/ApplicationShow.java
@@ -0,0 +1,9 @@
+package com.fxcode.twitterjavafx;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class ApplicationShow {
+    @FXML public Button Exit;
+    @FXML public Button login;
+}
